1000994a0aa3f94e59202f37f55ba27ba4dea426
CptNoHand <90973107+CptNoHand@users.noreply.github.com>
2022-08-19 21:28:19 +0200

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -425,23 +425,6 @@ RegisterNUICallback('TryCombination', function(data, cb)
                 takeAnim()
             else
                 TriggerEvent("police:SetCopAlert")
-                TriggerServerEvent('cd_dispatch:AddNotification', {
-                    job_table = {'police'}, 
-                    coords = data.coords,
-                    title = '10-15 - Store Robbery',
-                    message = 'A '..data.sex..' robbing a store at '..data.street, 
-                    flash = 0,
-                    unique_id = tostring(math.random(0000000,9999999)),
-                    blip = {
-                        sprite = 431, 
-                        scale = 1.2, 
-                        colour = 3,
-                        flashes = false, 
-                        text = '911 - Store Robbery',
-                        time = (5*60*1000),
-                        sound = 1,
-                    }
-                })
                 SetNuiFocus(false, false)
                 SendNUIMessage({
                     action = "closeKeypad",

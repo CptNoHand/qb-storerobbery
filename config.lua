e42b7b15f42255e7b2c9872b0117278c344f1517
CptNoHand <90973107+CptNoHand@users.noreply.github.com>
2022-09-29 00:22:29 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,10 +1,10 @@
 Config = {}
 
 Config.minEarn = 500
-Config.maxEarn = 1500
+Config.maxEarn = 2500
 Config.RegisterEarnings = math.random(Config.minEarn, Config.maxEarn)
-Config.MinimumStoreRobberyPolice = 0
-Config.resetTime = (60 * 1000) * 30
+Config.MinimumStoreRobberyPolice = 1
+Config.resetTime = (60 * 1000) * 10
 Config.tickInterval = 1000
 
 Config.Registers = {
@@ -49,21 +49,21 @@ Config.Safes = {
     [2] = {vector4(-1478.94, -375.5, 39.16,  229.5), type = "padlock", robbed = false, camId = 5},
     [3] = {vector4(-1220.85, -916.05, 11.32,  229.5), type = "padlock", robbed = false, camId = 6},
     [4] = {vector4(-709.74, -904.15, 19.21, 229.5), type = "keypad", robbed = false, camId = 7},
-    [5] = {vector3(28.21, -1339.14, 29.49), type = "keypad", robbed = false, camId = 8},
+    [5] = {vector3(31.19, -1339.27, 29.5), type = "keypad", robbed = false, camId = 8},
     [6] = {vector3(1126.77, -980.1, 45.41), type = "padlock", robbed = false, camId = 9},
     [7] = {vector3(1159.46, -314.05, 69.2), type = "keypad", robbed = false, camId = 10},
-    [8] = {vector3(378.17, 333.44, 103.56), type = "keypad", robbed = false, camId = 11},
+    [8] = {vector3(381.04, 332.61, 103.57), type = "keypad", robbed = false, camId = 11},
     [9] = {vector3(-1829.27, 798.76, 138.19), type = "keypad", robbed = false, camId = 12},
     [10] = {vector3(-2959.64, 387.08, 14.04), type = "padlock", robbed = false, camId = 13},
-    [11] = {vector3(-3047.88, 585.61, 7.9), type = "keypad", robbed = false, camId = 14},
-    [12] = {vector3(-3250.02, 1004.43, 12.83), type = "keypad", robbed = false, camId = 15},
-    [13] = {vector3(546.41, 2662.8, 42.15), type = "keypad", robbed = false, camId = 16},
+    [11] = {vector3(-3048.67, 588.48, 7.91), type = "keypad", robbed = false, camId = 14},
+    [12] = {vector3(-3249.67, 1007.45, 12.83), type = "keypad", robbed = false, camId = 15},
+    [13] = {vector3(543.74, 2662.65, 42.16), type = "keypad", robbed = false, camId = 16},
     [14] = {vector3(1169.31, 2717.79, 37.15), type = "padlock", robbed = false, camId = 17},
-    [15] = {vector3(2672.69, 3286.63, 55.24), type = "keypad", robbed = false, camId = 18},
-    [16] = {vector3(1959.26, 3748.92, 32.34), type = "keypad", robbed = false, camId = 19},
-    [17] = {vector3(1734.78, 6420.84, 35.03), type = "keypad", robbed = false, camId = 20},
+    [15] = {vector3(2674.34, 3289.23, 55.24), type = "keypad", robbed = false, camId = 18},
+    [16] = {vector3(1961.79, 3750.23, 32.34), type = "keypad", robbed = false, camId = 19},
+    [17] = {vector3(1737.45, 6419.46, 35.04), type = "keypad", robbed = false, camId = 20},
     [18] = {vector3(-168.40, 6318.80, 30.58), type = "padlock", robbed = false, camId = 27},
-    [19] = {vector3(168.95, 6644.74, 31.70), type = "keypad", robbed = false, camId = 30},
+    [19] = {vector3(170.96, 6642.44, 31.7), type = "keypad", robbed = false, camId = 30},
     [20] = {vector3(819.18, -774.57, 26.18), type = "padlock", robbed = false, camId = 31},
 }
 
